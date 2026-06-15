41179f6187501213bacca437c70cc450841850d7
noman291 <aanoman291@gmail.com>
2026-06-16 04:09:18 +0600

    signup page

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,7 +1,30 @@
 const register = () => {
   return (
-    <div>
-      <h1>register</h1>
+    <div className="container">
+      <div className="container-back-img"></div>
+      <div className="container-back-color"></div>
+      <div className="card">
+        <div className="card_title">
+          <h1>Create Account</h1>
+        </div>
+        <div className="form">
+          <form>
+            <div className="column">
+              <input type="text" placeholder="First Name" />
+              <input type="text" placeholder="Last Name" />
+            </div>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div className="card_terms">
+          <span>
+            Already have an account?
+            <a>Login Here</a>
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
